897182cb0d7b6756995816eb32b67b46c95d48d7
Tsegazeab <ttefferi@andrew.cmu.edu>
2026-04-17 10:49:52 +0300

    gestureExecution

--- cssr_system/gestureExecution/src/gestureExecutionApplication.cpp
+++ cssr_system/gestureExecution/src/gestureExecutionApplication.cpp
@@ -10,25 +10,25 @@
 * This program comes with ABSOLUTELY NO WARRANTY.
 *
 * <detailed functional description>
-* This module is responsible for hosting the service that executes the gestures on the robot. 
-* The module receives the gesture type, gesture ID, gesture duration, bow/nod angle, 
+* This module is responsible for hosting the service that executes the gestures on the robot.
+* The module receives the gesture type, gesture ID, gesture duration, bow/nod angle,
 * and the location in the world to pay attention/point to in x, y, z coordinates.
-* The module then executes the gesture based on the received parameters. 
+* The module then executes the gesture based on the received parameters.
 *
 * The module supports the execution of deictic, iconic, symbolic, bow, and nod gestures.
-* The iconic gestures currently supported are welcome and wave (goodbye) gestures. 
-* 
-* The module also supports the selection of the implementation platform (simulator or robot) 
+* The iconic gestures currently supported are welcome and wave (goodbye) gestures.
+*
+* The module also supports the selection of the implementation platform (simulator or robot)
 * and the interpolation type (linear or biological motion).
-* 
-* The module is implemented as a ROS service that receives the gesture parameters 
+*
+* The module is implemented as a ROS service that receives the gesture parameters
 * and returns the status of the gesture execution.
 *
 * The gestures could either be executed using linear velocity interpolation or a model of biological motion (minimum-jerk model).
 *
 * The module subscribes to the /sensor_msgs/joint_states topic to receive the joint states of the robot.
 * The module also subscribes to the /robotLocalization/pose topic to receive the coordinates of the robot in the world.
-* 
+*
 * The module is implemented in C++ and uses the ROS libraries for communication with the robot.
 * The module is part of the CSSR4A package and is used to execute gestures on the robot.
 *
@@ -53,7 +53,7 @@
 ...
 * Configuration File Parameters
 
-* Key                   |     Value 
+* Key                   |     Value
 * --------------------- |     -------------------
 * platform                    robot
 * interpolation               biological
@@ -74,13 +74,13 @@
 *
 * None
 ...
-* Advertised Service 
+* Advertised Service
 *
 * /gestureExecution/perform_gesture
-* 
+*
 ...
 * Invoked Service
-* 
+*
 * /overtAttention/set_mode
 *
 *
@@ -116,18 +116,16 @@
 *
 ...
 *
-* The clients can invoke the service by providing the gesture type, gesture ID, gesture duration, bow_nod angle, 
+* The clients can invoke the service by providing the gesture type, gesture ID, gesture duration, bow_nod angle,
 * and the location in the world to pay attention/point to in x, y, z coordinates.
 * The service will execute the gesture based on the received parameters and return the status of the gesture execution.
 *
 * Examples of calling the service is shown below:
-* ----- rosservice call /perform_gesture -- deictic 01 3000 25 3.6 2.5 0.82
+* ----- rosservice call /perform_gesture -- deictic 01 LEFT PALM_UPWARDS 3000 25 3.6 2.5 0.82
 * This will execute a pointing gesture with a duration of 3000 ms, and the location in the world to point to in x, y, z coordinates.
 *
-* ----- rosservice call /perform_gesture -- bow 01 3000 25 3.6 2.5 0.82
+* ----- rosservice call /perform_gesture -- bow 01 '' '' 3000 25 3.6 2.5 0.82
 * This will execute a pointing gesture with a duration of 3000 ms, and bow at an angle of 45 degrees.
-*
-*
 *
 *
 * Author: Adedayo Akinade, Carnegie Mellon University Africa
@@ -135,11 +133,17 @@
 * Date: January 10, 2025
 * Version: v1.0
 *
+* Author: Tsegazeab Tefferi, Carnegie Mellon University Africa
+* Email: ttefferi@andrew.cmu.edu
+* Date: April 15, 2026
+* Version: v1.0
+*
 */
 
 #include "gestureExecution/gestureExecutionInterface.h"
 
-int main(int argc, char **argv){
+int main(int argc, char** argv)
+{
     // Initialize the ROS node
     ros::init(argc, argv, "gestureExecution", ros::init_options::NoSigintHandler);
     ros::NodeHandle n;
@@ -152,21 +156,21 @@ int main(int argc, char **argv){
     }
 
     // Register the signal handler
-    signal(SIGINT, shut_down_handler);                                                      // The signal handler for the interrupt signal
+    signal(SIGINT, shut_down_handler);  // The signal handler for the interrupt signal
 
-    std::string copyright_message = node_name + ": " + std::string(SOFTWARE_VERSION) + 
+    std::string copyright_message = node_name + ": " + std::string(SOFTWARE_VERSION) +
                                     "\n\t\t\t\t\t\t   This project is funded by the African Engineering and Technology Network (Afretec)\n\t\t\t\t\t\t   Inclusive Digital Transformation Research Grant Programme. "
                                     "\n\t\t\t\t\t\t   Website: www.cssr4africa.org "
                                     "\n\t\t\t\t\t\t   This program comes with ABSOLUTELY NO WARRANTY.";
 
-    ROS_INFO("%s", copyright_message.c_str());                                                      // Print the copyright message
+    ROS_INFO("%s", copyright_message.c_str());  // Print the copyright message
 
-    ROS_INFO("%s: startup.", node_name.c_str());                                                    // Print startup message
+    ROS_INFO("%s: startup.", node_name.c_str());  // Print startup message
 
     // Initialize the node: Read configuration file and set the implementation platform, interpolation type, and topics file name
     int node_initialization_status = 0;
     node_initialization_status = initialize_node(&implementation_platform, &interpolation_type, &interpolation_mode, &gesture_descriptors_config_filename, &simulator_topics_filename, &robot_topics_filename, &topics_file_name, &verbose_mode_input, &verbose_mode);
-    if(node_initialization_status != 0){
+    if (node_initialization_status != 0) {
         ROS_ERROR("%s: initialization failed due to incorrect configuration.", node_name.c_str());
         shut_down_handler(0);
         return 0;
@@ -174,13 +178,13 @@ int main(int argc, char **argv){
 
     // Extract the joint_states topic and subscribe to the joint states topic
     std::string joint_states_topic;
-    if(extract_topic("JointStates", topics_file_name, &joint_states_topic)){
+    if (extract_topic("JointStates", topics_file_name, &joint_states_topic)) {
         ROS_ERROR("%s: error extracting the joint states topic.", node_name.c_str());
         shut_down_handler(0);
         return 0;
     }
     ROS_INFO("%s: subscribing to %s...", node_name.c_str(), joint_states_topic.c_str());
-    while(ros::ok() && !is_topic_available(joint_states_topic)){
+    while (ros::ok() && !is_topic_available(joint_states_topic)) {
         ROS_WARN_THROTTLE(INITIALIZATION_INFO_PERIOD, "%s: waiting for %s topic to be available...", node_name.c_str(), joint_states_topic.c_str());
         ros::Duration(1).sleep();
     }
@@ -189,29 +193,28 @@ int main(int argc, char **argv){
 
     // Create a publisher for the velocity commands
     std::string velocity_topic;
-    if(extract_topic("Wheels", topics_file_name, &velocity_topic)){
+    if (extract_topic("Wheels", topics_file_name, &velocity_topic)) {
         ROS_ERROR("%s: error extracting the wheels topic.", node_name.c_str());
         shut_down_handler(0);
         return 0;
     }
     ROS_INFO("%s: creating a publisher for the velocity commands...", node_name.c_str());
-    while(ros::ok() && !is_topic_available(velocity_topic)){
+    while (ros::ok() && !is_topic_available(velocity_topic)) {
         ROS_WARN_THROTTLE(INITIALIZATION_INFO_PERIOD, "%s: waiting for %s topic to be available...", node_name.c_str(), velocity_topic.c_str());
-        gesture_velocity_publisher = n.advertise<geometry_msgs::Twist>(velocity_topic.c_str(), 1, true);
         ros::Duration(1).sleep();
     }
-    // gesture_velocity_publisher = n.advertise<geometry_msgs::Twist>(velocity_topic.c_str(), 1000, true);
+    gesture_velocity_publisher = n.advertise<geometry_msgs::Twist>(velocity_topic.c_str(), 1000, true);
     ROS_INFO("%s: created a publisher for the velocity commands.", node_name.c_str());
 
     // Subscribe to the /robotLocalization/pose topic
     std::string robot_pose_topic;
-    if(extract_topic("RobotPose", topics_file_name, &robot_pose_topic)){
+    if (extract_topic("RobotPose", topics_file_name, &robot_pose_topic)) {
         ROS_ERROR("%s: error extracting the robot pose topic.", node_name.c_str());
         shut_down_handler(0);
         return 0;
     }
     ROS_INFO("%s: subscribing to robot pose topic...", node_name.c_str());
-    while(ros::ok() && !is_topic_available(robot_pose_topic)){
+    while (ros::ok() && !is_topic_available(robot_pose_topic)) {
         ROS_WARN_THROTTLE(INITIALIZATION_INFO_PERIOD, "%s: waiting for %s topic to be available...", node_name.c_str(), robot_pose_topic.c_str());
         ros::Duration(1).sleep();
     }
@@ -221,32 +224,37 @@ int main(int argc, char **argv){
     // Check if the overtAttention/set_mode service is available
     std::string overt_attention_service_name = "/overtAttention/set_mode";
     ROS_INFO("%s: creating client for the %s service...", node_name.c_str(), overt_attention_service_name.c_str());
-    while(ros::ok() && !is_service_available(overt_attention_service_name)){
+    while (ros::ok() && !is_service_available(overt_attention_service_name)) {
         ROS_WARN_THROTTLE(INITIALIZATION_INFO_PERIOD, "%s: waiting for %s service to be available...", node_name.c_str(), overt_attention_service_name.c_str());
         ros::Duration(1).sleep();
     }
     ROS_INFO("%s: created client for the %s service.", node_name.c_str(), overt_attention_service_name.c_str());
-    overt_attention_client = n.serviceClient<cssr_system::setMode>(overt_attention_service_name); 
+    overt_attention_client = n.serviceClient<cssr_system::setMode>(overt_attention_service_name);
 
     // Advertise the /gestureExecution/perform_gesture service
     std::string perform_gesture_service_name = "/gestureExecution/perform_gesture";
-    ros::ServiceServer perform_gesture_service;                                                     // perform gesture service object
+    ros::ServiceServer perform_gesture_service;  // perform gesture service object
     ROS_INFO("%s: advertising the %s service...", node_name.c_str(), perform_gesture_service_name.c_str());
-    while(ros::ok() && !perform_gesture_service){
+    while (ros::ok() && !perform_gesture_service) {
         perform_gesture_service = n.advertiseService(perform_gesture_service_name, execute_gesture);
         ros::Duration(1).sleep();
     }
     ROS_INFO("%s: advertised the %s service...", node_name.c_str(), perform_gesture_service_name.c_str());
-    
+
+    // Create the action server
+    actionlib::SimpleActionServer<cssr_system::gestureAction> as(n, "/gestureExecution/perform_gesture", boost::bind(&execute_gesture_action, _1, &as), false);
+
+    // Start the action server
+    as.start();
+    ROS_INFO("Action server started. Waiting for goals...");
 
     // Print the node ready message after initialization complete
     ROS_INFO("%s: initialization complete...", node_name.c_str());
-    node_initialized = true;                                                                        // Set the node initialization flag to true
+    node_initialized = true;  // Set the node initialization flag to true
 
-
-    while(ros::ok()){
+    while (ros::ok()) {
         ros::spinOnce();
-        ROS_INFO_THROTTLE(OPERATION_INFO_PERIOD, "%s: running...", node_name.c_str());              // Print heartbeat message
+        ROS_INFO_THROTTLE(OPERATION_INFO_PERIOD, "%s: running...", node_name.c_str());  // Print heartbeat message
     }
 
     return 0;
